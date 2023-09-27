1c15df8b469549ae48578150fb0d972a0e637565
asif2212 <asifmd2212@gamil.com>
2023-09-27 12:22:15 +0600

    updated

--- config/app.php
+++ config/app.php
@@ -166,6 +166,7 @@ return [
         UniSharp\LaravelFilemanager\LaravelFilemanagerServiceProvider::class,
         Intervention\Image\ImageServiceProvider::class,
         Barryvdh\DomPDF\ServiceProvider::class,
+        Srmklive\PayPal\Providers\PayPalServiceProvider::class,
         Laravel\Socialite\SocialiteServiceProvider::class,
         /*
          * Package Service Providers...
@@ -234,7 +235,7 @@ return [
         // unisharp
         'Image' => Intervention\Image\Facades\Image::class,
         'PDF' => Barryvdh\DomPDF\Facade::class,
-
+        'PayPal' => Srmklive\PayPal\Facades\PayPal::class,
         'Socialite' => Laravel\Socialite\Facades\Socialite::class,
 
     ],
